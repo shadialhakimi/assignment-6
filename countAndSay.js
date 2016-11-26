b9ca2afc91fed688075de366d8c88de3417e83b6
Shadi Alhakimi <shadi_15_5@hotmail.com>
2016-11-27 00:27:53 +0100

    Update countAndSay.js

--- countAndSay.js
+++ countAndSay.js
@@ -1,11 +1,14 @@
-var numbers = [];
-numbers[0] ="1";
-limit=prompt("please enter the limit value",6);
+
+var numbers = [];// this array contain the list of values for the sequence
+numbers[0] ="1"; //the starter value of the sequence
+limit=prompt("please enter the limit value",6);//the end value of the sequence
+//this is the outer loop for the whole sequence
 for(var k=1;k<=limit;k++)
     {
 var res = numbers[k-1].split("");
 var counter=1;
 var result="";
+        //this is the inner loop for calculating each value in the sequence
 for (var i=0; i<res.length; i++) {
     if(res[i]===res[i+1])
         {
@@ -23,6 +26,6 @@ for (var i=0; i<res.length; i++) {
 }
         numbers[k]=result;
     }
-
+//this is the loop for printing the sequence in the console
 for(item in numbers)
 console.log(numbers[item]);
